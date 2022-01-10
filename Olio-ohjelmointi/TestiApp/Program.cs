289f9a0acb5072110c21692b56236ec5ededfe98
Jami Leppänen <jami.leppanen@student.careeria.fi>
2022-01-10 12:58:57 +0200

    Koira Esim.

--- Olio-ohjelmointi/TestiApp/Program.cs
+++ Olio-ohjelmointi/TestiApp/Program.cs
@@ -6,7 +6,21 @@ namespace TestiApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Koira koira1 = new Koira();//Ensimmäinen koira olio
+            koira1.nimi = "Seppo";
+            koira1.ikä = 5;
+            koira1.rotu = "Saksanpaimenkoira";
+
+
+            koira1.Hauku();
+            koira1.TulostaData();
+
+            Koira koira2 = new Koira();
+            koira2.nimi = "asd";
+            koira2.ikä = 2;
+            koira2.rotu = "Mopsi";
+
+            koira2.TulostaData();
         }
     }
 }
