210c1ef27dfc10faab5707dddd786974a0a4aa0f
Olli Juhola <juhola.olli@gmail.com>
2022-01-10 12:06:12 +0200

    Projekti luotu

---
+++ OlioOhjelmointi/TestiApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestiApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
