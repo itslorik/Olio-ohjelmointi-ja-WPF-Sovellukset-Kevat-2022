b3e0e6660dc63241b826ef34d738c0b81842dfea
Olli Juhola <juhola.olli@gmail.com>
2022-01-10 14:05:59 +0200

    Harjoitus 2

---
+++ OlioOhjelmointi/Harjoitus 2/Opiskelija.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_2
+{
+    class Opiskelija
+    {
+        private string nimi;
+        private string opiskelijaID;
+        private int opintopisteet;
+
+        public Opiskelija(string _nimi, string _opiskelijaID, int _opintopisteet)
+        {
+            nimi = _nimi;
+            opiskelijaID = _opiskelijaID;
+            opintopisteet = _opintopisteet;
+        }
+
+        public void TulostaData()
+        {
+            Console.WriteLine(nimi + ". ID: " + opiskelijaID + ". Opintopisteet: " + opintopisteet);
+        }
+
+        public void MuokkaaOpintopisteitä(int määrä)
+        {
+            // Lisätään opiskelijalle opintopisteitä "määrä" -parametrin verran
+            opintopisteet += määrä;
+        }
+    }
+}
---
+++ OlioOhjelmointi/Harjoitus 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Harjoitus_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Opiskelija opiskelija1 = new Opiskelija("Pekka", "12345", 0);
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteitä(10);
+            opiskelija1.MuokkaaOpintopisteitä(15);
+            opiskelija1.TulostaData();
+        }
+    }
+}
