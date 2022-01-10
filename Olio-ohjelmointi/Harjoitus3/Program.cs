5c4d00764f5293c3a89bade6c74f3843bdaa0966
Jami Leppänen <jami.leppanen@student.careeria.fi>
2022-01-10 14:37:43 +0200

    Harjoitus3

--- Olio-ohjelmointi/Harjoitus3/Program.cs
+++ Olio-ohjelmointi/Harjoitus3/Program.cs
@@ -7,12 +7,20 @@ namespace Harjoitus3
         static void Main(string[] args)
         {
             Kiuas kiuas1 = new Kiuas("Kastor", 60, 40);
-            Kiuas kiuas2 = new Kiuas("Helo", 90, 10);
-            Kiuas kiuas3 = new Kiuas("Veto", 30, 70);
 
             kiuas1.NäytäTiedot();
+            kiuas1.Tila = true;
+            kiuas1.NäytäTiedot();
+            kiuas1.SäädäLämpötila(100);
+            kiuas1.NäytäTiedot();
+
+            Kiuas kiuas2 = new Kiuas("Helo", 90, 10);
             kiuas2.NäytäTiedot();
-            kiuas3.NäytäTiedot();
+            kiuas2.Tila = true;
+            kiuas2.NäytäTiedot();
+            kiuas2.SäädäLämpötila(100);
+            kiuas2.NäytäTiedot();
+            
         }
     }
 }
