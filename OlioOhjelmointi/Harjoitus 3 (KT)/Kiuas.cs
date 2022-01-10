1c39331fc877c5d9aec6202249faac28ee3287ef
Olli Juhola <juhola.olli@gmail.com>
2022-01-10 14:37:22 +0200

    Harjoitus 3

---
+++ OlioOhjelmointi/Harjoitus 3 (KT)/Kiuas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_3__KT_
+{
+    class Kiuas
+    {
+        private string Nimi;
+        private int Lämpötila;
+        private int Kosteus;
+        public bool Tila = false;
+
+        public Kiuas(string _nimi, int _lämpötila, int _kosteus)
+        {
+            Nimi = _nimi;
+            Lämpötila = _lämpötila;
+            Kosteus = _kosteus;
+        }
+
+        public void NäytäTiedot()
+        {
+            if (Tila) // <-- if (Tila == true)
+            {
+                Console.WriteLine("Kiuas " + Nimi + " on päällä");
+                Console.WriteLine("Lämpötila = " + Lämpötila);
+                Console.WriteLine("Kosteus = " + Kosteus);
+            } else
+            {
+                Console.WriteLine("Kiuas " + Nimi + " ei ole päällä");
+            }
+        }
+
+        public void SäädäLämpötila(int uusiLämpötila)
+        {
+            Lämpötila = uusiLämpötila;
+        }
+    }
+}
---
+++ OlioOhjelmointi/Harjoitus 3 (KT)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Harjoitus_3__KT_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas harvia = new Kiuas("Harvia", 90, 50);
+
+            harvia.NäytäTiedot();
+            harvia.Tila = true;
+            harvia.NäytäTiedot();
+            harvia.SäädäLämpötila(100);
+            harvia.NäytäTiedot();
+        }
+    }
+}
