7e638f45379eefac249c328c31ba2d824dab230c
Jami Leppänen <jami.leppanen@student.careeria.fi>
2022-01-10 14:27:55 +0200

    Harjoitus3

--- Olio-ohjelmointi/Harjoitus3/Program.cs
+++ Olio-ohjelmointi/Harjoitus3/Program.cs
@@ -6,7 +6,13 @@ namespace Harjoitus3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Kiuas kiuas1 = new Kiuas("Kastor", 60, 40);
+            Kiuas kiuas2 = new Kiuas("Helo", 90, 10);
+            Kiuas kiuas3 = new Kiuas("Veto", 30, 70);
+
+            kiuas1.NäytäTiedot();
+            kiuas2.NäytäTiedot();
+            kiuas3.NäytäTiedot();
         }
     }
 }
