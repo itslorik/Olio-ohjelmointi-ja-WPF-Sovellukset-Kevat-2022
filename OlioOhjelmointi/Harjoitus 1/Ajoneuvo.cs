670d639174c961fe689abfc31474dd8bac98a298
Olli Juhola <juhola.olli@gmail.com>
2022-01-10 13:39:04 +0200

    Harjoitus 1

---
+++ OlioOhjelmointi/Harjoitus 1/Ajoneuvo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_1
+{
+    class Ajoneuvo
+    {
+        public string Nimi;
+        public int Nopeus;
+        public int Renkaat;
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Ajoneuvon nimi: " + Nimi);
+            Console.WriteLine("Ajoneuvon nopeus: " + Nopeus);
+            Console.WriteLine("Ajoneuvon renkaiden määrä: " + Renkaat);
+        }
+
+        public string ToString()
+        {
+            return "Ajoneuvo: " + Nimi + ". Nopeus: " + Nopeus + ". Renkaiden määrä: " + Renkaat;
+        }
+    }
+}
---
+++ OlioOhjelmointi/Harjoitus 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Harjoitus_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ajoneuvo auto = new Ajoneuvo();
+            auto.Nimi = "Toyota Corolla";
+            auto.Nopeus = 420;
+            auto.Renkaat = 4;
+
+            auto.TulostaData();
+
+            Ajoneuvo vene = new Ajoneuvo();
+            vene.Nimi = "Buster";
+            vene.Nopeus = 100;
+            vene.Renkaat = 0;
+
+            vene.TulostaData();
+        }
+    }
+}
