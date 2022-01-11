c3639153e941b81ba98ac965e0404564e9171624
Jami Leppänen <jami.leppanen@student.careeria.fi>
2022-01-11 14:55:47 +0200

    Kesken

---
+++ Olio-ohjelmointi/Harjoitus 9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Harjoitus_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio radio = new Radio(0, 9);
+            string syöte;
+
+            while (true)
+            {
+                Console.WriteLine("Radio Numero.." + )
+            }
+        }
+    }
+}
---
+++ Olio-ohjelmointi/Harjoitus 9/Radio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus_9
+{
+    class Radio
+    {
+
+        private int minTaajus;
+        private int maxTaajus;
+        private int minÄäni;
+        private int maxÄäni;
+
+        private int nykyinenTaajus;
+
+
+        public Radio(int minTaajus, int maxTaajus)
+        {
+            this.minTaajus = minTaajus;
+            this.maxTaajus = maxTaajus;
+
+            nykyinenTaajus = minTaajus;
+        }
+        public int NykyinenKerros
+        {
+            get => nykyinenTaajus;
+            set
+            {
+                if (value > maxTaajus || value < minTaajus)
+                {
+                    Console.WriteLine("Virheellinen Kerros!");
+                }
+                else
+                {
+                    nykyinenTaajus = value;
+                }
+            }
+        }
+    }
+}
