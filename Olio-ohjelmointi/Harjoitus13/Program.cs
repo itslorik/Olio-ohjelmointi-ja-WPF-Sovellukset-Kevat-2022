ed151b8de9c8c6e3a9bbb9443a547bfdb4863afc
Careeria <Careeria@DESKTOP-V3DJQEL>
2022-01-17 15:12:42 +0200

    kesken

--- Olio-ohjelmointi/Harjoitus13/Program.cs
+++ Olio-ohjelmointi/Harjoitus13/Program.cs
@@ -13,7 +13,42 @@ namespace Harjoitus13
             joukkue1.TulostaTiedot();
             pelaajat1.TulostaData();
 
+            Console.WriteLine("--------------");
+            Console.WriteLine("HaePelaaja");
+            Console.WriteLine(" LisääPelaaja: ");
+            Console.WriteLine(" PoistaPelaaja: ");
+            Console.WriteLine(" LisääPelaaja: ");
+            Console.WriteLine("1.  LisääPelaaja: ");
+        }
 
+
+
+        static void HaePelaaja()
+        {
+            Pelaajat uusipelaajat = new Pelaajat();
+            while (true)
+            {
+                Console.WriteLine("Anna uudelle opiskelijalle tiedot: ");
+                Console.WriteLine("Etunimi: ");
+                uusipelaajat.Etunimi = Console.ReadLine();
+                Console.WriteLine("Sukunimi: ");
+                uusipelaajat.Sukunimi = Console.ReadLine();
+                Console.WriteLine("R: ");
+                uusipelaajat.PelaajanNumero = Console.ReadLine();
+                Console.WriteLine("OpiskelijaID");
+                uusiOpiskelija.OpiskelijaNumero = Convert.ToInt32(Console.ReadLine());
+
+                arvoit uudestaan
+                if (opiskelijat.ContainsKey(uusiOpiskelija.OpiskelijaNumero))
+                {
+                    Console.WriteLine("Opiskelija sanakirjassa on opiskelija samalla ID:llä");
+                    continue;
+                }
+                else
+                {
+                    opiskelijat.Add(uusiOpiskelija.OpiskelijaNumero, uusiOpiskelija);
+                    break;
+                }
+            }
         }
     }
-}
