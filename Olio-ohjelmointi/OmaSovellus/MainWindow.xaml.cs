d189918f11582552db79b683c974633139b3c467
Careeria <Careeria@DESKTOP-V3DJQEL>
2022-01-25 13:33:19 +0200

    OmaSovellusTehtyy

---
+++ Olio-ohjelmointi/OmaSovellus/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OmaSovellus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Page> Sivut = new List<Page>();
+        int Nykyinensivu = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Sivut.Add(new Page1());
+            Sivut.Add(new Page2());
+            frame_Main.NavigationService.Navigate(Sivut[0]);
+
+        }
+
+        private void btn_SeuraavaSivu_Click(object sender, RoutedEventArgs e)
+        {
+            Nykyinensivu += 1;
+            if (Nykyinensivu >= Sivut.Count)
+            {
+                Nykyinensivu = Sivut.Count - 1;
+            }
+            frame_Main.NavigationService.Navigate(Sivut[Nykyinensivu]);
+
+        }
+
+        private void btn_Sivu2_Click(object sender, RoutedEventArgs e)
+        {
+            Nykyinensivu -= 1;
+            if (Nykyinensivu < 0)
+            {
+                Nykyinensivu = 0;
+            }
+            frame_Main.NavigationService.Navigate(Sivut[Nykyinensivu]);
+
+        }
+    }
+}
---
+++ Olio-ohjelmointi/OmaSovellus/Page2.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OmaSovellus
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Input_Kentä_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_Pekka == null)
+            {
+                return;
+            }
+            txt_Pekka.Text = Input_Kentä.Text;
+            if (Input_Kentä.Text == "Sauli Niinistö")
+            {
+                txt_Pekka.Text += ". Oiken!";
+            }
+            else if (Input_Kentä.Text != " ")
+            {
+                
+                txt_Pekka.Text += "";
+            }
+        }
+
+        private void Input_Koulu_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txt_Seppo == null)
+            {
+                return;
+            }
+            txt_Seppo.Text = Input_Koulu.Text;
+            
+        }
+    }
+}
